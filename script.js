f9948552e101a71c203e524a8423c2d2656d9c42
Shashank <ssjha0542@gmail.com>
2021-08-04 20:25:41 +0530

    added the record timing and applied some filters

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let contraints={video:true,audio:true};
 let mediaRecorder;
 let chunks=[];
 let captureBtn=document.querySelector("#capture-btn");
+let clearObj;
+let timingELem = document.querySelector("#timing");
 videoRecorderBtn.addEventListener("click",function(){
     if(mediaRecorder!=undefined){
         if(recordState==false){
@@ -12,12 +14,14 @@ videoRecorderBtn.addEventListener("click",function(){
         
         mediaRecorder.start();
         videoRecorderBtn.classList.add("record-animation");
+        startCounting();
 
     }
     else{
         recordState=false;
         mediaRecorder.stop();
         videoRecorderBtn.classList.remove("record-animation");
+        stopCounting();
     }
 }
     
@@ -41,6 +45,7 @@ navigator.mediaDevices.getUserMedia(contraints).then(function(mediaStream){
 }).catch(function(err){
     console.log(err);
 })
+
 captureBtn.addEventListener("click",function(){
     captureBtn.classList.add("capture-animation");
     //creating  a canvas equal to the video height and with
@@ -62,3 +67,19 @@ captureBtn.addEventListener("click",function(){
         captureBtn.classList.remove("capture-animation");
     },1000);
 })
+function startCounting() {
+    timingELem.classList.add("timing-active");
+    let timeCount = 0;
+    clearObj = setInterval(function () {
+        let seconds = (timeCount % 60) < 10 ? `0${timeCount % 60}` : `${timeCount % 60}`;
+        let minutes = (timeCount / 60) < 10 ? `0${Number.parseInt(timeCount / 60)}` : `${Number.parseInt(timeCount / 60)}`;
+        let hours = (timeCount / 3600) < 10 ? `0${Number.parseInt(timeCount / 3600)}` : `${Number.parseInt(timeCount / 3600)}`;
+        timingELem.innerText = `${hours}:${minutes}:${seconds}`;
+        timeCount++;
+    }, 1000);
+}
+function stopCounting() {
+    timingELem.classList.remove("timing-active");
+    timingELem.innerText = "00: 00: 00";
+    clearInterval(clearObj);
+}
